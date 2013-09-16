cfd68978c84ec0f1d425f46ff1086370d7421e76
djrunner <danielbreidenbach@gmail.com>
2013-09-16 08:14:32 -0400

    fixed list in php, added sans-serif in css

--- index.php
+++ index.php
@@ -5,11 +5,11 @@
 	<h3>Name: Daniel Breidenbach</h3>
 
 	<h3>Other related information</h3>
-       <li>
-          <ul>Place of birth: New York City</ul>
-          <ul>Undergraduate degree: Philosophy</ul>
-          <ul>Favorate Baseball team: NY Mets</ul>
-          <ul>Favorate board game: Agricola</ul>
-        </li>
+       <ul>
+          <li>Place of birth: New York City</li>
+          <li>Undergraduate degree: Philosophy</li>
+          <li>Favorate Baseball team: NY Mets</li>
+          <li>Favorate board game: Agricola</li>
+        </ul>
 
     <h3>Code editor used: Sublime Text 2</h3>

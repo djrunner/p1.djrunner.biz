2bcd4b010319deb7cac618f73b54e5e390588fdc
djrunner <danielbreidenbach@gmail.com>
2013-09-16 09:10:51 -0400

    added more infromation in php.index, more color in css file

--- index.php
+++ index.php
@@ -1,15 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Project 1: Workflow setup and essential HTML/CSS deomonstration</title>
 <link rel="stylesheet" type="text/css" href="index.css" />
+</head>
+<body>
 
-<h1>Welcome to P1<h>
+<h1>Project 1<h1>
 
-	<h3>Name: Daniel Breidenbach</h3>
+	<h2>Who I am/Why I'm here</h2>
+	   <ul>
+          <li>My Name is Daniel Breidenbach.</li>
+          <li>I hold an undergratuate Degree in Philosophy from Northeastern.</li>
+          <li>I am taking this class for graduate credit towards a Masters in Softwear Engineering.</li>
+          <li>/I am taking this course concurrently with CSCI E-22.</li>
+       </ul>
 
-	<h3>Other related information</h3>
+	<h2>Other related information</h2>
        <ul>
           <li>Place of birth: New York City</li>
-          <li>Undergraduate degree: Philosophy</li>
+          <li>Favorate activites: Running, boardgaming</li>
           <li>Favorate Baseball team: NY Mets</li>
           <li>Favorate board game: Agricola</li>
         </ul>
 
-    <h3>Code editor used: Sublime Text 2</h3>
+    <h2>Code editor/Browser info</h2>
+       <ul>
+    	   <li>I am suning Sublime Text 2 as my test editor.</li>
+           <li>I am using Google Chrome as my browser.</li>
+           <li>I will test this site on Internet Explorer</li>
+       </ul>
+
+</body>
+</html>

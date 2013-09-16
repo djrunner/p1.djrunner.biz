e450e93e79ddd6428ccd487a62bb25d28c15aa78
djrunner <danielbreidenbach@gmail.com>
2013-09-15 22:18:51 -0400

    modified index.php

--- index.php
+++ index.php
@@ -1,1 +1,13 @@
 <h1>Welcome to P1<h>
+
+	<h3>Name: Daniel Breidenbach</h3>
+
+	<h3>Other related information</h3>
+       <li>
+          <ul>Place of birth: New York City</ul>
+          <ul>Undergraduate degree: Philosophy</ul>
+          <ul>Favorate Baseball team: NY Mets</ul>
+          <ul>Favorate board game: Agricola</ul>
+        </li>
+
+    <h3>Code editor used: Sublime Text 2</h3>

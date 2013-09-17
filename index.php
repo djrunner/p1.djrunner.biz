9997ca993928a64a4abe8fe79925a49f00a55ccf
djrunner <danielbreidenbach@gmail.com>
2013-09-17 09:20:46 -0400

    fixed css, added photo

--- index.php
+++ index.php
@@ -6,30 +6,38 @@
 </head>
 <body>
 
-<h1>Project 1<h1>
-
+<div id="me">
 	<h2>Who I am/Why I'm here</h2>
 	   <ul>
           <li>My Name is Daniel Breidenbach.</li>
-          <li>I hold an undergratuate Degree in Philosophy from Northeastern.</li>
-          <li>I am taking this class for graduate credit towards a Masters in Softwear Engineering.</li>
-          <li>/I am taking this course concurrently with CSCI E-22.</li>
+          <li>I hold an undergraduate Degree in Philosophy from Northeastern.</li>
+          <li>I am taking this class for graduate credit towards a Master's in Software Engineering.</li>
+          <li>I am taking this course concurrently with CSCI E-22.</li>
        </ul>
+</div>
 
-	<h2>Other related information</h2>
+<div id="Favorite">
+	<h2>Favorite Things</h2>
        <ul>
-          <li>Place of birth: New York City</li>
-          <li>Favorate activites: Running, boardgaming</li>
-          <li>Favorate Baseball team: NY Mets</li>
-          <li>Favorate board game: Agricola</li>
+          <li>Favorite Food: NY Pizza</li>
+          <li>Favorite activates: Running, board gaming</li>
+          <li>Favorite Baseball team: NY Mets</li>
+          <li>Favorite board game: Agricola</li>
         </ul>
+</div>
 
+<div id="programs">
     <h2>Code editor/Browser info</h2>
        <ul>
-    	   <li>I am suning Sublime Text 2 as my test editor.</li>
+    	   <li>I am using Sublime Text 2 as my test editor.</li>
            <li>I am using Google Chrome as my browser.</li>
-           <li>I will test this site on Internet Explorer</li>
+           <li>I will test this site on Internet Explorer.</li>
        </ul>
+</div>
+
+<div id="pic">
+   <img src="Project1Pic.png" alt =""/>
+</div>
 
 </body>
 </html>
